9e87b794a1017af97d9d942ef540d07cd1043638
CMMH0210 <114383220+CMMH0210@users.noreply.github.com>
2022-09-29 09:19:32 +0530

    Add files via upload

---
+++ simplilearn2022/simplilearn2022/src/main/java/com2/simplilearn2022/Simplilearn2022Application.java
@@ -0,0 +1,13 @@
+package com2.simplilearn2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simplilearn2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Simplilearn2022Application.class, args);
+	}
+
+}
---
+++ simplilearn2022/simplilearn2022/src/test/java/com2/simplilearn2022/Simplilearn2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com2.simplilearn2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Simplilearn2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
